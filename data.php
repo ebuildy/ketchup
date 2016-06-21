dbbbaf9793bac6dcf965067645532ec91d51cd2a
Jcchemin <jcchemin06@gmail.com>
2016-06-21 15:08:08 +0200

    Update data.php

--- data.php
+++ data.php
@@ -1,43 +1,43 @@
 <?php 
 
 $data = array();
-$data[0]['title'] = "Why The Legend of Zelda: Breath of the Wild was the Best Game at E3 2016";
+$data[0]['title'] = "Why The Legend of <b>Zelda</b>: Breath of the Wild was the Best Game at E3 2016";
 $data[0]['description'] = "One week ago, I played Zelda: Breath of the Wild and thought it was Nintendo's most ambitious game in years. It was the first game I played at this year's E3, and it ";
 $data[0]['thumbnail'] = "http://cloud.attackofthefanboy.com/wp-content/uploads/2016/02/The-Legend-of-Zelda-Wii-U.jpg?fb38c8";
 $data[0]['time'] = "Today";
 $data[0]['link'] = "http://www.eurogamer.net/articles/2016-06-20-we-played-zelda-breath-of-the-wild-four-times-and-still-didnt-see-it-all";
 
-$data[1]['title'] = "Bayern Munich defender Mats Hummels explains why he snubbed Manchester United transfer";
+$data[1]['title'] = "<b>Bayern Munich</b> defender Mats Hummels explains why he snubbed Manchester United transfer";
 $data[1]['description'] = "One week ago, I played Zelda: Breath of the Wild and thought it was Nintendo's most ambitious game in years. It was the first game I played at this year's E3, and it ";
 $data[1]['thumbnail'] = "http://i3.mirror.co.uk/incoming/article7759087.ece/ALTERNATES/s615/Liverpool-v-Borussia-Dortmund-UEFA-Europa-League-Quarter-Final-Second-Leg";
 $data[1]['time'] = "Today";
 $data[1]['link'] = "http://www.mirror.co.uk/sport/football/news/bayern-munich-defender-mats-hummels-8243416";
 
-$data[2]['title'] = "Samsung Galaxy S7 vs OnePlus 3 specs comparison: Latest flagship from OnePlus 3 has outstanding hardware";
+$data[2]['title'] = "Samsung Galaxy S7 vs <b>OnePlus</b> 3 specs comparison: Latest flagship from OnePlus 3 has outstanding hardware";
 $data[2]['description'] = "One week ago, I played Zelda: Breath of the Wild and thought it was Nintendo's most ambitious game in years. It was the first game I played at this year's E3, and it ";
 $data[2]['thumbnail'] = "http://d.ecumenicalnews.com/full/17449/galaxy-s7-vs-oneplus-3.jpg?w=590&h=446&l=50&t=40";
 $data[2]['time'] = "Today";
 $data[2]['link'] = "http://www.ecumenicalnews.com/article/samsung-galaxy-s7-vs-oneplus-3-latest-flagship-from-oneplus-3-has-outstanding-hardware/46321.htm";
 
-$data[3]['title'] = "Why The Legend of Zelda: Breath of the Wild was the Best Game at E3 2016";
+$data[3]['title'] = "Leigh Halfpenny can write Welsh <b>rugby</b> history for <b>Toulon</b> in the Top 14 final against Dan Carter's Racing 92 at Nou Camp";
 $data[3]['description'] = "One week ago, I played Zelda: Breath of the Wild and thought it was Nintendo's most ambitious game in years. It was the first game I played at this year's E3, and it ";
 $data[3]['thumbnail'] = "http://i2.walesonline.co.uk/incoming/article11498044.ece/ALTERNATES/s615/GettyImages-541075328.jpg";
 $data[3]['time'] = "Yesterday";
 $data[3]['link'] = "http://www.walesonline.co.uk/sport/rugby/leigh-halfpenny-can-write-welsh-11498370";
 
-$data[4]['title'] = "Apple explains why it totally redesigned how the Apple Watch works";
+$data[4]['title'] = "<b>Apple</b> explains why it totally redesigned how the <b>Apple</b> Watch works";
 $data[4]['description'] = "One week ago, I played Zelda: Breath of the Wild and thought it was Nintendo's most ambitious game in years. It was the first game I played at this year's E3, and it ";
 $data[4]['thumbnail'] = "http://cloud.attackofthefanboy.com/wp-content/uploads/2016/02/The-Legend-of-Zelda-Wii-U.jpg?fb38c8";
 $data[4]['time'] = "Yesterday";
 $data[4]['link'] = "http://www.businessinsider.de/apple-watchos-30-is-a-major-interface-redesign-2016-6?r=UK&IR=T";
 
-$data[5]['title'] = "Docker builds container orchestration right into its core Docker Engine";
+$data[5]['title'] = "<b>Docker</b> builds container orchestration right into its core <b>Docker</b> Engine";
 $data[5]['description'] = "One week ago, I played Zelda: Breath of the Wild and thought it was Nintendo's most ambitious game in years. It was the first game I played at this year's E3, and it ";
 $data[5]['thumbnail'] = "https://tctechcrunch2011.files.wordpress.com/2016/06/img_20160619_175158.jpg?w=738";
 $data[5]['time'] = "Yesterday";
 $data[5]['link'] = "https://techcrunch.com/2016/06/20/docker-builds-swarm-right-into-its-core-core-tools/";
 
-$data[6]['title'] = "Daft Punk and Jarvis Cocker have contributed to a Stanley Kubrick exhibition";
+$data[6]['title'] = "<b>Daft Punk</b> and Jarvis Cocker have contributed to a Stanley Kubrick exhibition";
 $data[6]['description'] = "One week ago, I played Zelda: Breath of the Wild and thought it was Nintendo's most ambitious game in years. It was the first game I played at this year's E3, and it ";
 $data[6]['thumbnail'] = "http://www.mixmag.net/assets/uploads/images/_columns2/kubrick.jpg";
 $data[6]['time'] = "3 days ago";
@@ -49,13 +49,13 @@ $data[7]['thumbnail'] = "http://www.cbronline.com/BRHome/ResizeImage/_Uploads_Ne
 $data[7]['time'] = "5 days ago";
 $data[7]['link'] = "http://www.cbronline.com/news/big-data/analytics/exclusive-how-the-guardian-online-is-breaking-news-with-real-time-big-data-analytics-4922632";
 
-$data[8]['title'] = "Lille : l'ISEN fabrique une main en exosquelette imprimable en 3D";
+$data[8]['title'] = "<b>Axel Springer</b> buys eMarketer to reduce digital ad dependency";
 $data[8]['description'] = "One week ago, I played Zelda: Breath of the Wild and thought it was Nintendo's most ambitious game in years. It was the first game I played at this year's E3, and it ";
-$data[8]['thumbnail'] = "http://ne.rosselcdn.net/sites/default/files/imagecache/vdn_photo_principale_article/articles/ophotos/20160518/884079216_bc85f78e4f33f57e4b28affb55eef3c8_79214409_B978690956Z.1_20160518182242_000_GTU6QAIHG.1-0.jpg";
+$data[8]['thumbnail'] = "https://i.guim.co.uk/img/media/c9ed219060003449c793a845ede537b470701db7/0_1051_1314_788/master/1314.jpg?w=620&q=55&auto=format&usm=12&fit=max&s=ce226e1b43625e414a46154baec9b92a";
 $data[8]['time'] = "1 week ago";
-$data[8]['link'] = "http://www.nordeclair.fr/info-locale/lille-l-isen-fabrique-une-main-en-exosquelette-imprimable-en-3d-jna49b0n1133793";
+$data[8]['link'] = "http://www.theguardian.com/media/2016/jun/10/axel-springer-buys-emarketer-to-reduce-digital-ad-dependency";
 
-$data[9]['title'] = "Qwant Music : un moteur de recherche spécialisé en musique";
+$data[9]['title'] = "<b>Qwant</b> Music : un moteur de recherche spécialisé en musique";
 $data[9]['description'] = "One week ago, I played Zelda: Breath of the Wild and thought it was Nintendo's most ambitious game in years. It was the first game I played at this year's E3, and it ";
 $data[9]['thumbnail'] = "http://fr.cdn.v5.futura-sciences.com/builds/images/thumbs/a/aae8a0799f_qwantMusic_logo.jpg";
 $data[9]['time'] = "1 week ago";
