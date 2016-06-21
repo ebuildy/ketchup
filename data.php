6c422aa043e567531b9ee1754072189ae47f3774
Jcchemin <jcchemin06@gmail.com>
2016-06-21 15:14:49 +0200

    Update data.php

--- data.php
+++ data.php
@@ -2,19 +2,19 @@
 
 $data = array();
 $data[0]['title'] = "Why The Legend of <b>Zelda</b>: Breath of the Wild was the Best Game at E3 2016";
-$data[0]['description'] = "One week ago, I played Zelda: Breath of the Wild and thought it was Nintendo's most ambitious game in years. It was the first game I played at this year's E3...";
+$data[0]['description'] = "One week ago, I played <b>Zelda</b>: Breath of the Wild and thought it was <b>Nintendo</b>'s most ambitious game in years. It was the first game I played at this year's E3...";
 $data[0]['thumbnail'] = "http://cloud.attackofthefanboy.com/wp-content/uploads/2016/02/The-Legend-of-Zelda-Wii-U.jpg?fb38c8";
 $data[0]['time'] = "Today";
 $data[0]['link'] = "http://www.eurogamer.net/articles/2016-06-20-we-played-zelda-breath-of-the-wild-four-times-and-still-didnt-see-it-all";
 
 $data[1]['title'] = "<b>Bayern Munich</b> defender Mats Hummels explains why he snubbed Manchester United transfer";
-$data[1]['description'] = "United were interested in Hummels in 2014 but he stayed at Borussia Dortmund for two more years before joining Bayern last month";
+$data[1]['description'] = "United were interested in Hummels in 2014 but he stayed at Borussia Dortmund for two more years before joining <b>Bayern</b> last month";
 $data[1]['thumbnail'] = "http://i3.mirror.co.uk/incoming/article7759087.ece/ALTERNATES/s615/Liverpool-v-Borussia-Dortmund-UEFA-Europa-League-Quarter-Final-Second-Leg";
 $data[1]['time'] = "Today";
 $data[1]['link'] = "http://www.mirror.co.uk/sport/football/news/bayern-munich-defender-mats-hummels-8243416";
 
 $data[2]['title'] = "Samsung Galaxy S7 vs <b>OnePlus</b> 3 specs comparison: Latest flagship from OnePlus 3 has outstanding hardware";
-$data[2]['description'] = "Samsung and Apple are quickly being challenged by up-and-coming Asian smartphone manufacturers and one of them is OnePlus. Recently, the company brought out its flagship OnePlus 3 device...";
+$data[2]['description'] = "Samsung and <b>Apple</b> are quickly being challenged by up-and-coming Asian smartphone manufacturers and one of them is <b>OnePlus</b>. Recently, the company brought out its flagship OnePlus 3 device...";
 $data[2]['thumbnail'] = "http://d.ecumenicalnews.com/full/17449/galaxy-s7-vs-oneplus-3.jpg?w=590&h=446&l=50&t=40";
 $data[2]['time'] = "Today";
 $data[2]['link'] = "http://www.ecumenicalnews.com/article/samsung-galaxy-s7-vs-oneplus-3-latest-flagship-from-oneplus-3-has-outstanding-hardware/46321.htm";
@@ -26,13 +26,13 @@ $data[3]['time'] = "Yesterday";
 $data[3]['link'] = "http://www.walesonline.co.uk/sport/rugby/leigh-halfpenny-can-write-welsh-11498370";
 
 $data[4]['title'] = "<b>Apple</b> explains why it totally redesigned how the <b>Apple</b> Watch works";
-$data[4]['description'] = "Users will receive a free update to WatchOS 3 this fall, which is essentially a wholesale redesign of the Apple Watch interface";
+$data[4]['description'] = "Users will receive a free update to WatchOS 3 this fall, which is essentially a wholesale redesign of the <b>Apple</b> Watch interface";
 $data[4]['thumbnail'] = "http://cloud.attackofthefanboy.com/wp-content/uploads/2016/02/The-Legend-of-Zelda-Wii-U.jpg?fb38c8";
 $data[4]['time'] = "Yesterday";
 $data[4]['link'] = "http://www.businessinsider.de/apple-watchos-30-is-a-major-interface-redesign-2016-6?r=UK&IR=T";
 
 $data[5]['title'] = "<b>Docker</b> builds container orchestration right into its core <b>Docker</b> Engine";
-$data[5]['description'] = "Docker, which is hosting its sold-out developer conference in Seattle this week, today announced a major addition to its core Docker Engine";
+$data[5]['description'] = "<b>Docker</b>, which is hosting its sold-out developer conference in Seattle this week, today announced a major addition to its core <b>Docker</b> Engine";
 $data[5]['thumbnail'] = "https://tctechcrunch2011.files.wordpress.com/2016/06/img_20160619_175158.jpg?w=738";
 $data[5]['time'] = "Yesterday";
 $data[5]['link'] = "https://techcrunch.com/2016/06/20/docker-builds-swarm-right-into-its-core-core-tools/";
@@ -43,8 +43,8 @@ $data[6]['thumbnail'] = "http://www.mixmag.net/assets/uploads/images/_columns2/k
 $data[6]['time'] = "3 days ago";
 $data[6]['link'] = "http://www.mixmag.net/read/daft-punk-are-daydreaming-with-stanley-kubrick-at-london-exhibition-news";
 
-$data[7]['title'] = "How the Guardian online is breaking news with real-time big data analytics";
-$data[7]['description'] = "Transforming a hackaday project into the backbone of how journalists work in a data driven organisation";
+$data[7]['title'] = "How the Guardian online is breaking news with real-time <b>big data</b> analytics";
+$data[7]['description'] = "Transforming a <b>hackaday</b> project into the backbone of how journalists work in a data driven organisation";
 $data[7]['thumbnail'] = "http://www.cbronline.com/BRHome/ResizeImage/_Uploads_NewsArticle_4922632_main.JPG/1440/828/Details/0/0";
 $data[7]['time'] = "5 days ago";
 $data[7]['link'] = "http://www.cbronline.com/news/big-data/analytics/exclusive-how-the-guardian-online-is-breaking-news-with-real-time-big-data-analytics-4922632";
@@ -56,7 +56,7 @@ $data[8]['time'] = "1 week ago";
 $data[8]['link'] = "http://www.theguardian.com/media/2016/jun/10/axel-springer-buys-emarketer-to-reduce-digital-ad-dependency";
 
 $data[9]['title'] = "<b>Qwant</b> Music : un moteur de recherche spécialisé en musique";
-$data[9]['description'] = "Après une version Junior spécialement adaptée aux enfants de 6 à 12 ans, le moteur de recherche « européen » vient de lancer Qwant Music";
+$data[9]['description'] = "Après une version Junior spécialement adaptée aux enfants de 6 à 12 ans, le moteur de recherche « européen » vient de lancer <b>Qwant</b> Music";
 $data[9]['thumbnail'] = "http://fr.cdn.v5.futura-sciences.com/builds/images/thumbs/a/aae8a0799f_qwantMusic_logo.jpg";
 $data[9]['time'] = "1 week ago";
 $data[9]['link'] = "http://www.futura-sciences.com/magazines/high-tech/infos/actu/d/internet-qwant-music-moteur-recherche-specialise-musique-63116/";
